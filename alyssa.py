0ff2cac9a669c17aa8eb089c09806d169cfddf1b
alexchanningUF <148847262+alexchanningUF@users.noreply.github.com>
2023-10-25 10:45:04 -0500

    Update alyssa.py
    added decode and changed main function a bit

--- alyssa.py
+++ alyssa.py
@@ -9,21 +9,34 @@ def password_encoder(encode):
         password += formula
     return password
 
+def decoder(input1): ## alex
+    output = ""
+    for digit in input1:
+        x = str((int(digit) - 3) % 10)
+        output += x
+
+    return output
 
 def menu():
-    print('Menu')
-    print('-------------')
-    print('1. Encode\n2. Decode\n3 Quit')
-    option = input('Please enter an option: ')
     res = ''
-    while option != '3':
+    res2 = ''
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode\n2. Decode\n3. Quit')
+        option = input('Please enter an option: ')
         if option == '1':
             encode = (input('Please enter your password to encode: '))
             res = password_encoder(encode)
             print('Your password has been encoded and stored!')
-        # Partner to help with else section
+            # Partner to help with else section
         elif option == '2':
-            pass
+            res2 = decoder(res)
+            print(f"The encoded password is {res}, and the original password is {res2}.")
+        elif option == '3':
+            break
+            
+            
 
 
 if __name__ == '__main__':
